b038fdb57fd0c2ea1809f7c11b7793942d3c294a
legnbass <charmentroutv@gmail.com>
2023-02-18 13:35:04 -0500

    Added css and stuff like that

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,9 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, button, div, text, span)
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (class)
 
 
 
@@ -13,46 +14,40 @@ main : Program () Model Msg
 main =
   Browser.sandbox { init = init, update = update, view = view }
 
-
-
 -- MODEL
-
-
 type alias Model = Int
 
 
 init : Model
-init =
-  0
-
-
+init = 0
 
 -- UPDATE
-
-
-type Msg
-  = Increment
-  | Decrement
-
+type Msg = Regenerate
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Increment ->
-      model + 1
-
-    Decrement ->
-      model - 1
-
-
+    Regenerate -> model
 
 -- VIEW
-
-
 view : Model -> Html Msg
-view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+view model = 
+  div [] [
+    div [ class "clear" ] [],
+    div [ class "card" ] [
+      div [ class "headers"] [
+        div [] [ span [] [text "B"] ],
+        div [] [ span [] [text "I"] ],
+        div [] [ span [] [text "N"] ],
+        div [] [ span [] [text "G"] ],
+        div [] [ span [] [text "O"] ]
+      ],
+      div [ class "column 1"] [],
+      div [ class "column 2"] [],
+      div [ class "column 3"] [],
+      div [ class "column 4"] [],
+      div [ class "column 5"] []
+    ],
+    div [class "temp" ] [],
+    button [ onClick Regenerate ] [text "Generate New Card"]
+  ]
