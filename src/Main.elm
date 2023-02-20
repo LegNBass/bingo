d0f473a483bc0f94ed444c456091fcc19aac8ac7
legnbass <charmentroutv@gmail.com>
2023-02-19 21:18:15 -0500

    Refactored randomization strategy

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,14 @@
 module Main exposing (..)
 
 import Browser
+import Random exposing (Generator)
+import Random.Set exposing (set)
+import Dict exposing (Dict)
 import Html exposing (Html, button, div, text, span)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (class)
-import Html.Lazy exposing (lazy)
+import Set exposing (Set)
+-- import Html.Lazy exposing (lazy)
 
 
 -- CONSTANTS
@@ -14,22 +18,52 @@ bingo = String.toList "BINGO"
 -- MAIN
 main : Program () Model Msg
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+  Browser.element { init = init, subscriptions = subscriptions, update = update, view = view }
 
 -- MODEL
-type alias Model = List String
+type alias Model = {
+    b : (List Int),
+    i : (List Int),
+    n : (List Int),
+    g : (List Int),
+    o : (List Int) 
+  }
 
 
-init : Model
-init = []
+generateCard: Generator Model
+generateCard = 
+  let
+    genUnique: List Int -> Generator Int 
+    genUnique used = Random.int 1 75 |> Random.andThen (
+      \n -> if List.member n used then
+              genUnique used -- Tail Recursion possible in elm?
+            else
+            -- Is there such thing as a generator literal
+              (Random.int n n)
+      )
+
+    genRow: Generator (List Int)
+    genRow = Random.list 5 <| genUnique []
+  in 
+    Random.map5 Model genRow genRow genRow genRow genRow 
+
+
+init : () -> (Model, Cmd Msg)
+init _ = (Model [] [] [] [] [], Cmd.none)
 
 -- UPDATE
-type Msg = Regenerate
+type Msg = Generate | NewCard Model
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Regenerate -> model
+    Generate -> (model, Random.generate NewCard generateCard) --TODO: generate a new card
+    NewCard ll -> (ll, Cmd.none) --TODO: Add newly random numbers to the model
+
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
 
 -- VIEW
 
@@ -37,7 +71,7 @@ mkHeaders: Char -> Html msg
 mkHeaders c = div [] [ span [] [ text (String.fromChar c) ]]
 
 buildBoxes: Char -> List (Html msg)
-buildBoxes c = List.range 1 5
+buildBoxes c = List.range 1 5 -- TODO: Get the number list from the model here and use them in the lambda
               |> List.map (\_ -> div [ class "number" ] [ span [] [text <| String.fromChar c] ] )
 
 mkColumn: Int -> Char-> Model -> Html msg
@@ -46,11 +80,10 @@ mkColumn i c model = div [ class ("column " ++ (String.fromInt i)) ] (buildBoxes
 view : Model -> Html Msg
 view model = 
   div [] [
-    button [ onClick Regenerate ] [text "Generate New Card"],
+    button [ onClick Generate ] [text "Generate New Card"],
     div [ class "clear" ] [],
     div [ class "card" ] (
-      (div [ class "headers"] (List.map mkHeaders bingo))
-      :: (List.indexedMap (\i c -> mkColumn i c model) bingo)
+      (div [ class "headers"] (List.map mkHeaders bingo)) :: (List.indexedMap (\i c -> mkColumn i c model) bingo)
     ),
     div [class "temp" ] []
   ]
